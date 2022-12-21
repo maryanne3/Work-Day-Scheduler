d1e95e9cc9414887a748462fa7f725c3fed10c2e
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2022-12-21 22:27:48 +0000

    changes in script js

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Work-Day-Scheduler
 A calendar application that allows a user to save events for each hour of the day.
 
+## Technologies used in the making 
 
+## Deployed page 
+## Deployed git hub link 
+## The purpose of this application 
+This application grants the user to schedule thier day to day life/meetings 
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,55 @@
-//list of variables 
-$("#currentDay").text(moment().format('dddd, MMMM D, YYYY, h:mm:ss a'));
-$().button('toggle')
+//start of document & list of variables 
+$(document).ready(function(){
+$("#currentDay").text(moment().format('dddd Do MMMM, YYYY HH:mm:ss'));
+$(".btn-group").button("toggle");
+
+//Timeblock variables 
+  $("#timeblock").val(moment().format('HH:mm:ss'));
+  $("#timeblock").change(function() {
+    $("#timeblock").val(moment().format('HH:mm:ss'));
+  });
+  $("#timeblock").trigger("change");
+
+// Checking if on current time or passed it 
+  if(moment().format('HH:mm:ss') == $("#timeblock").val()) {
+    $("#timeblock").val(moment().format('HH:mm:ss'));
+    $("#timeblock").trigger("change");
+  }
+
+});
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//end of document
